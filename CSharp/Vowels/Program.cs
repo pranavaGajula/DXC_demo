193b2288fc440549a560f62b21f84ae2fff16a3a
Gajula <pranava.gajula@dxc.com>
2023-04-20 13:35:17 +0530

    initial commit

--- CSharp/Vowels/Program.cs
+++ CSharp/Vowels/Program.cs
@@ -1,7 +1,7 @@
 ﻿char ch;
 Console.WriteLine("enter a character");
 ch = Convert.ToChar(Console.ReadLine());
-if (ch == 'a'|| ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
 {
     Console.WriteLine("Character is Vowel");
 }
